2386cbbd0707d198016bf08338a196d8bb4f005a
Hampus Hjortsberg <hampushjortsberg@gmail.com>
2024-06-12 15:48:51 +0000

    Pre testing save point

--- src/pages/auth/SignInForm.js
+++ src/pages/auth/SignInForm.js
@@ -15,9 +15,11 @@ import styles from '../../styles/SignInUpForm.module.css';
 import btnStyles from '../../styles/Button.module.css';
 import appStyles from '../../App.module.css';
 import { useSetCurrentUser } from '../../contexts/CurrentUserContext';
+import { useRedirect } from '../../hooks/useRedirect';
 
 function SignInForm() {
   const setCurrentUser = useSetCurrentUser();
+  useRedirect('loggedIn');
 
   const [signInData, setSignInData] = useState({
     username: '',
@@ -33,9 +35,8 @@ function SignInForm() {
 
     try {
       const { data } = await axios.post('/dj-rest-auth/login/', signInData);
-      console.log(data.user)
       setCurrentUser(data.user);
-      history.push('/');
+      history.goBack();
     } catch (err) {
       setErrors(err.response?.data);
     }
